9a38c088d3aa07b1dbe0a48c2869a8583838017e
DESKTOP-251G08N\dougl <douglasmg7@gmail.com>
2024-08-23 10:45:12 -0300

    Lesson 3.4 finished.

--- Assets/Scritps/PlayerController.cs
+++ Assets/Scritps/PlayerController.cs
@@ -8,9 +8,16 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public bool isOnGround = true;
     public bool gameOver;
+    public ParticleSystem exploxionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
 
     Rigidbody playerRb;
     Animator playerAnim;
+    AudioSource audioSource;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +26,17 @@ public class PlayerController : MonoBehaviour
         Physics.gravity *= gravityModifier;
 
         playerAnim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Jump.
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
+            audioSource.PlayOneShot(jumpSound, 1f);
+            dirtParticle.Stop();
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
 
@@ -35,15 +46,22 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Ground"))
+        // Allow jump again.
+        if (other.gameObject.CompareTag("Ground") && !gameOver)
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
-        if (other.gameObject.CompareTag("Obstacle"))
+
+        // Crash.
+        if (other.gameObject.CompareTag("Obstacle") && !gameOver)
         {
+            audioSource.PlayOneShot(crashSound, 1f);
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            dirtParticle.Stop();
+            exploxionParticle.Play();
             Debug.Log("Game Over!");
         }
     }
